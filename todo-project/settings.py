e3e69a0f1afe8fa61fa6f03cc69205a6ad1d0687
Rana fathima🪐 <ismailpunnol@MacBook-Pro.local>
2026-04-09 21:02:16 +0530

    Fix: Add Vercel deployment URL to ALLOWED_HOSTS

--- todo-project/settings.py
+++ todo-project/settings.py
@@ -24,12 +24,8 @@ SECRET_KEY = 'django-insecure-pg85i8t&veg5uezkq&oq!8zxgi%)zt9cg&06pn*zj(rj7j08x0
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "todo-project-seven-mu.vercel.app",
-]
+# settings.py
+ALLOWED_HOSTS = ['*']  # This is the "easy" fix to test if it works
 
 
 # Application definition
